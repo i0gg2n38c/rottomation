2a5ba6a66d06b365db177102c143e61548e996e8
root <i0gg2n38c@mozmail.com>
2025-04-28 00:24:33 +0000

    some cleanup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
-require_relative 'support/requires'
 require_relative '../config/configuration'
 require_relative '../lib/rottomation'
+
+lib = File.expand_path('../lib', __dir__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 
 RSpec.configure do |config|
   config.before do
@@ -10,9 +12,4 @@ RSpec.configure do |config|
 
   config.after do
   end
-end
-
-def get_client(logger)
-  Rottomation::Mastodon::MastodonClient.new logger: logger, token: 'TOKEN_AUTH_GOES_HERE',
-                                           hostname: 'https://{TARGET_WEBBED_SITE}'
 end
--- spec/support/requires.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-# Add lib to load path
-lib = File.expand_path('../lib', __dir__)
-$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
